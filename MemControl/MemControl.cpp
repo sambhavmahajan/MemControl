704eb2f62786b71de6c3c4538a2ffff3373391d1
Sambhav Mahajan <sambhavmahajan123@gmail.com>
2024-01-13 16:19:10 +0530

    Refactor code to implement a linked list structure for dynamic block creation and introduce a generic Head class. Also, add a template function for searching blocks based on the input type.

--- MemControl/MemControl.cpp
+++ MemControl/MemControl.cpp
@@ -7,32 +7,59 @@ struct Block {
     Block* next;
 };
 template<typename T>
-T* newBlock(T *val)
+T* newBlock(T* val)
 {
     Block<T> newblock = new Block<T>;
     newblock->address = val;
     newblock->next = nullptr;
-    return newBlock;
+    return newblock;
+}
+template<typename T>
+class Head {
+public:
+    T* item;
+};
+template <typename T>
+void searchBlocks(T Target)
+{
+    T* i = 0;
+    while(i < 0x7fffffff)
+    {
+        if (*i == Target)
+        {
+
+        }
+        ++i;
+    }
 }
 int main()
 {
     char type[7];
+    type[6] = '\0';
     cout << "Enter type of variable: ";
     cin >> type;
     cout << "Enter Value: ";
     if (strcmp(type,"int") == 0)
     {
-        
+        int t;
+        cin >> t;
+        searchBlocks<int>(t);
     }else if (strcmp(type, "float") == 0)
     {
-        
+        float t;
+        cin >> t;
+        searchBlocks<float>(t);
     }else if (strcmp(type, "double") == 0)
     {
-        
+        double t;
+        cin >> t;
+        searchBlocks<double>(t);
     }
-    else if (strcmp(type, "char"))
+    else if (strcmp(type, "char") == 0)
     {
-        
+        char t;
+        cin >> t;
+        searchBlocks<char>(t);
     }
     else {
         cout << "Fail!";
