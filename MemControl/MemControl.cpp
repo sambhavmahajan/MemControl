3b7153d855b92841689458e833b3694e80a941b1
Sambhav Mahajan <sambhavmahajan123@gmail.com>
2024-01-13 15:49:49 +0530

    newBlock function

--- MemControl/MemControl.cpp
+++ MemControl/MemControl.cpp
@@ -6,10 +6,37 @@ struct Block {
     T* address;
     Block* next;
 };
+template<typename T>
+T* newBlock(T *val)
+{
+    Block<T> newblock = new Block<T>;
+    newblock->address = val;
+    newblock->next = nullptr;
+    return newBlock;
+}
 int main()
 {
-    char Type[7];
-    cout << "Enter type of variable";
-    cin >> Type;
+    char type[7];
+    cout << "Enter type of variable: ";
+    cin >> type;
+    cout << "Enter Value: ";
+    if (strcmp(type,"int") == 0)
+    {
+        
+    }else if (strcmp(type, "float") == 0)
+    {
+        
+    }else if (strcmp(type, "double") == 0)
+    {
+        
+    }
+    else if (strcmp(type, "char"))
+    {
+        
+    }
+    else {
+        cout << "Fail!";
+        return 1;
+    }
     return 0;
 }
